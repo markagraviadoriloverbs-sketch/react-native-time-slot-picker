bd582deb1b1ee707f8c8eba2d3717274b64e4929
markagraviadoriloverbs-sketch <mark.agraviador.iloverbs@gmail.com>
2026-04-01 08:28:16 +0800

    Changed start color in data.ts

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,6 +1,6 @@
 import { theme } from './theme';
 
-export const defaultActiveColor = theme.colors.primary900;
+export const defaultActiveColor = theme.colors.success300;
 export const defaultTimeSlotWidth = 96;
 
 export const defaultDayNames = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
